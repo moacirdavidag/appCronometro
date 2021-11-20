35f967c08a7496ecfa76e76e23dcd022c8a1c32c
moadavidag <moacirdavidag@gmail.com>
2021-11-20 13:02:35 -0300

    Correção de bug no cronômetro

--- js/temporizador.js
+++ js/temporizador.js
@@ -4,10 +4,7 @@ var interfaceTemporizador = document.getElementById("temporizador");
 var abrirTemporizador = document.querySelector("#abrirTemporizador");
 var abrirRelogio = document.querySelector("#abrirRelogio");
 var abrirCronometro = document.querySelector("#abrirCronometro");
-abrirTemporizador.addEventListener("click", mostrarInterface);
 
-abrirRelogio.addEventListener("click", mostrarInterface);
-abrirRelogio.addEventListener("click", mostrarInterface);
 abrirTemporizador.addEventListener("click", mostrarInterface);
 
 function mostrarInterface() {
@@ -16,4 +13,38 @@ function mostrarInterface() {
         interfaceTemporizador.style.display = "block";
         interfaceRelogio.style.display = "none";
     }
+}
+
+var horaInput = document.querySelector("#valorHora");
+var minutoInput = document.querySelector("#valorMinuto"); 
+var segundoInput = document.querySelector("#valorSegundo");
+
+
+var iniciarTemporizador = document.querySelector("#iniciarTemporizador");
+var pausarTemporizador = document.querySelector("#pausarTemporizador");
+var pararTemporizador = document.querySelector("#pararTemporizador");
+
+
+iniciarTemporizador.addEventListener("click", temporizador);
+
+var temp; 
+
+
+function temporizador() {
+    pausarTemporizador.style.display = "block";
+    pararTemporizador.style.display = "block";
+
+    temporizador = setInterval(() => {
+        let horas = Number(hora.value), minutos = Number(minuto.value), segundos = Number(segundo.value);
+       if(horas > 0) {
+           segundos--;
+           if(segundos == 0) {
+               segundos = 60;
+               minutos--;
+               if(minutos == 0) {
+                   horas--;
+               }
+           }
+       }
+    }, 1000);
 }
